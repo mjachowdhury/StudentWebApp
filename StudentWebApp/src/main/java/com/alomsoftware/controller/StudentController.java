f7dcb22fa84ab50d51e74fd7a65e72008f2c7253
mjachowdhury <study.computing2016@gmail.com>
2017-08-26 02:30:14 +0100

    @Controller tag
    @Controller tag StudnetControlelr class
    -setupForm method was created

--- StudentWebApp/src/main/java/com/alomsoftware/controller/StudentController.java
+++ StudentWebApp/src/main/java/com/alomsoftware/controller/StudentController.java
@@ -1,5 +1,27 @@
 package com.alomsoftware.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alomsoftware.model.Student;
+import com.alomsoftware.service.StudentService;
+
+@Controller
 public class StudentController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@RequestMapping("/index")
+	public String setupForm(Map<String, Object> map){
+		Student student = new Student();
+		
+		map.put("studnet", student);
+		map.put("studentList", studentService.getAllStudnet());
+		return "studnet";		
+	}
 
 }
